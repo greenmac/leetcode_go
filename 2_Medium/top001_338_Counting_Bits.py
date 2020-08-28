d684a39095b1a8db8e84558b7068c0a424502ea8
greenmac <alwaysmac@msn.com>
2020-08-28 16:58:27 +0800

    top001_338_Counting_Bits

---
+++ 2_Medium/top001_338_Counting_Bits.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/counting-bits/
+# https://leetcode.com/problems/counting-bits/discuss/815835/Python-solution-using-a-pattern.
+
+'''
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example 1:
+
+Input: 2
+Output: [0,1,1]
+Example 2:
+
+Input: 5
+Output: [0,1,1,2,1,2]
+'''
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        out = [0]
+        for i in range(1, num+1):
+            out.append(out[i//2] + i%2) 
+        return out
+
+num = 2
+result = Solution().countBits(num)
+print(result)
