65289f571a278988bc1c5d5887c40e6e2d426ead
greenmac <alwaysmac@msn.com>
2020-08-30 12:08:33 +0800

    top004_094_Binary_Tree_Inorder_Traversal

---
+++ 2_Medium/top004_094_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+# https://ithelp.ithome.com.tw/articles/10213280
+
+'''
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
+
+
+# root = [1,None,2,3]
+# result = Solution().inorderTraversal(root)
+# print(result)
--- test.py
+++ test.py
@@ -12,8 +12,14 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(hello())
 loop.close()
 '''
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = a + b
+# print(c)
 
-a = [1, 2, 3]
-b = [4, 5, 6]
-c = a + b
-print(c)
+
+list1 = ['Google', 'Runoob', 'Taobao']
+# list_pop=list1.pop(1)
+list_pop=list1.pop()
+print ("删除的项为 :", list_pop)
+print ("列表现在为 : ", list1)
